8fd79021cf624db12faa3c873de020dc4591fa7f
Justin Chan <justin.chan.1@stonybrook.edu>
2022-08-01 10:56:11 -0400

    Adding the next activity

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.helloworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,6 +17,16 @@ class MainActivity : AppCompatActivity() {
             Log.v("Hello Worlld","Button clicked")
             Toast.makeText(this, "This button was clicked.", Toast.LENGTH_SHORT).show()
         }
+
+        val nextButton=findViewById<Button>(R.id.nextButton)
+        nextButton.setOnClickListener(){
+            Toast.makeText(this, "Next page.", Toast.LENGTH_SHORT).show()
+            openNextScreen()
+        }
     }
+     fun openNextScreen(){
+         val intent = Intent(this, SecondActivity::class.java)
+         startActivity(intent)
+     }
 
 }
---
+++ app/src/main/java/com/example/helloworld/SecondActivity.kt
@@ -0,0 +1,18 @@
+package com.example.helloworld
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import org.w3c.dom.Text
+
+
+class SecondActivity {
+     fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        val button= findViewById<Text>(R.id.textView)
+    }
+}
